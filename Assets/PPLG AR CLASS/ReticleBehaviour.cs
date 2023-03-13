b437d1788734de7b6072d487e975efd509cc0251
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-03-13 09:04:46 +0700

    first commit

---
+++ Assets/PPLG AR CLASS/ReticleBehaviour.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ReticleBehaviour : MonoBehaviour
+{
+    public GameObject Child;
+    public DrivingSurfaceManager DrivingSurfaceManager;
+
+    public ARPlane CurrentPlane;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Child = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        DrivingSurfaceManager.RaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinBounds);
+        
+        CurrentPlane = null;
+        ARRaycastHit? hit = null;
+        if (hits.Count > 0)
+        {
+            // If you don't have a locked plane already...
+            var lockedPlane = DrivingSurfaceManager.LockedPlane;
+            hit = lockedPlane == null
+                // ... use the first hit in `hits`.
+                ? hits[0]
+                // Otherwise use the locked plane, if it's there.
+                : hits.SingleOrDefault(x => x.trackableId == lockedPlane.trackableId);
+        }
+        
+        if (hit.HasValue)
+        {
+            CurrentPlane = DrivingSurfaceManager.PlaneManager.GetPlane(hit.Value.trackableId);
+            // Move this reticle to the location of the hit.
+            transform.position = hit.Value.pose.position;
+        }
+        Child.SetActive(CurrentPlane != null);
+        
+    }
+}
